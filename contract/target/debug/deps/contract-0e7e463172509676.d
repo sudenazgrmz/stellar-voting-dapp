7a613ef2c17d55c6b69739a5ee40698d82d4a003
sudenazgmrz <gormezsudenaz2@gmail.com>
2025-05-31 22:45:28 +0300

    first commit

---
+++ contract/target/debug/deps/contract-0e7e463172509676.d
@@ -0,0 +1,5 @@
+/Users/sude/Desktop/proje/contract/target/debug/deps/libcontract-0e7e463172509676.rmeta: src/lib.rs
+
+/Users/sude/Desktop/proje/contract/target/debug/deps/contract-0e7e463172509676.d: src/lib.rs
+
+src/lib.rs:
---
+++ contract/target/debug/deps/contract-bd14e30e30e3c022.d
@@ -0,0 +1,5 @@
+/Users/sude/Desktop/proje/contract/target/debug/deps/libcontract-bd14e30e30e3c022.rmeta: src/lib.rs
+
+/Users/sude/Desktop/proje/contract/target/debug/deps/contract-bd14e30e30e3c022.d: src/lib.rs
+
+src/lib.rs:
